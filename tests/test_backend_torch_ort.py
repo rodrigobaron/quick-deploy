50948b5f4025ea672fb950c0461c95b6be76d19b
Rodrigo Baron <baron.rodrigo0@gmail.com>
2022-01-05 00:03:49 -0300

    Issue/backend tests (#12)
    * add: github actions
    
    * fix typo
    
    * try build itself
    
    * use dev packages
    
    * add: backend tests
    
    * black

---
+++ tests/test_backend_torch_ort.py
@@ -0,0 +1,23 @@
+import torch
+
+from quick_deploy.backend.torch_ort import torch_convert_onnx
+from quick_deploy.utils import parse_torch_input
+import mock
+
+
+@mock.patch("quick_deploy.backend.torch_ort.torch.onnx.export")
+def test_torch_convert_onnx(m):
+    inputs_pytorch, inputs_onnx = parse_torch_input(shape=(1,), batch_size=1, use_cuda=False)
+    model = torch.nn.Module()
+    torch_convert_onnx(model=model, output_path='/tmp/torch', inputs_pytorch=inputs_pytorch, verbose=False)
+
+    name, args, kwargs = m.mock_calls[0]
+
+    assert (model,) == args
+    assert (1, 1) == kwargs['args'][0].shape
+    assert '/tmp/torch' == kwargs['f']
+    assert 12 == kwargs['opset_version']
+    assert kwargs['do_constant_folding']
+    assert ['input'] == kwargs['input_names']
+    assert ['output'] == kwargs['output_names']
+    assert {'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}} == kwargs['dynamic_axes']
